6597c56ab514432707e3ddeb128846630c620194
Soham Thaker <thakersoham3@gmail.com>
2023-11-20 12:20:35 -0500

    scoped the index.js code into a function invoking the function at the end

--- index.js
+++ index.js
@@ -6,53 +6,58 @@ import {
   checkArgs,
 } from './src/utils/helper.js';
 import fs from 'fs';
-let args = process.argv.slice(2);
 
-// Check for config flag
-const configFlagIndex = args.findIndex(
-  (arg) => arg === '-c' || arg === '--config'
-);
+function runTil() {
+  let args = process.argv.slice(2);
 
-//Check if user has passed `-c` or `--config` flag followed by a file path.
-if (configFlagIndex !== -1 && args[configFlagIndex + 1]) {
-  const configFilePath = args[configFlagIndex + 1];
+  // Check for config flag
+  const configFlagIndex = args.findIndex(
+    (arg) => arg === '-c' || arg === '--config'
+  );
 
-  if (!fs.existsSync(configFilePath)) {
-    console.error('Configuration file not found:', configFilePath);
-    process.exit(-1);
+  //Check if user has passed `-c` or `--config` flag followed by a file path.
+  if (configFlagIndex !== -1 && args[configFlagIndex + 1]) {
+    const configFilePath = args[configFlagIndex + 1];
+
+    if (!fs.existsSync(configFilePath)) {
+      console.error('Configuration file not found:', configFilePath);
+      process.exit(-1);
+    }
+
+    const config = readAndParseTomlConfig(configFilePath);
+
+    // Check for version flag within the TOML config file
+    if (config.version === true) {
+      printVersionAndProgramName();
+    }
+
+    // Check for help flag within the TOML config file
+    if (config.help === true) {
+      printHelpMenu();
+    }
+
+    const inputPath = config.input || '';
+    const outputPath = config.output || './til';
+    sanitizeInputCommand([inputPath, '-o', outputPath]);
+    process.exit(0);
   }
 
-  const config = readAndParseTomlConfig(configFilePath);
+  checkArgs(args);
 
-  // Check for version flag within the TOML config file
-  if (config.version === true) {
+  let command = args[0];
+
+  // User has provided a valid version command. Output the name of the program & version number.
+  if (command === '-v' || command === '--version') {
     printVersionAndProgramName();
   }
-
-  // Check for help flag within the TOML config file
-  if (config.help === true) {
+  // User has provided a valid help command. Output the help menu.
+  else if (command === '-h' || command === '--help') {
     printHelpMenu();
   }
-
-  const inputPath = config.input || '';
-  const outputPath = config.output || './til';
-  sanitizeInputCommand([inputPath, '-o', outputPath]);
-  process.exit(0);
+  // User has provided a valid input command. Sanitize the input command.
+  else {
+    sanitizeInputCommand(args);
+  }
 }
 
-checkArgs(args);
-
-let command = args[0];
-
-// User has provided a valid version command. Output the name of the program & version number.
-if (command === '-v' || command === '--version') {
-  printVersionAndProgramName();
-}
-// User has provided a valid help command. Output the help menu.
-else if (command === '-h' || command === '--help') {
-  printHelpMenu();
-}
-// User has provided a valid input command. Sanitize the input command.
-else {
-  sanitizeInputCommand(args);
-}
+runTil();
